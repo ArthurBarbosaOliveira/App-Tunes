2614deebd6ab9bd42ece928201018a53d3ead9ff
Arthur <arthurbarbosa93@gmail.com>
2022-03-08 17:04:04 -0300

    Requisito 2 ok

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    isDisabled: true,
+    inputUser: '',
+    carregando: false,
+    redirect: false,
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.validade);
+  }
+
+  validade = () => {
+    const { inputUser } = this.state;
+    const MinChar = 3;
+    if (inputUser.length >= MinChar) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    const { inputUser } = this.state;
+    this.setState({ carregando: true }, async () => {
+      await createUser({ name: inputUser });
+      this.setState({ carregando: false, redirect: true });
+    });
+  }
+
   render() {
+    const { isDisabled, inputUser, carregando, redirect } = this.state;
+    if (carregando) return <span>Carregando...</span>;
+    if (redirect) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
-        <h1>LOGIN</h1>
+        <form onSubmit={ this.handleClick }>
+          <label htmlFor="inputUser">
+            <input
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+              value={ inputUser }
+              name="inputUser"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
