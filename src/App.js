59c772e44ea26efebd03fefdcc4e53053e4ed5c5
Arthur <arthurbarbosa93@gmail.com>
2022-03-08 15:25:30 -0300

    Requisito 01 ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <>
+        <Login />
+        <Search />
+        <Album />
+        <Favorites />
+        <Profile />
+        <ProfileEdit />
+        <NotFound />
+      </>
+    );
   }
 }
 
